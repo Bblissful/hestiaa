da281ebdfef5bcba032dbd9ab80ee04315f0d18a
Muhammad Yahaya <101328329+Muhammad235@users.noreply.github.com>
2023-06-30 22:58:22 +0100

    added js file

---
+++ app.js
@@ -0,0 +1,22 @@
+let form = document.getElementById("myForm");
+let cuisine1 = document.getElementById('cuisine1')
+let cuisine2 = document.getElementById('cuisine2')
+let location1 = document.getElementById('location1')
+let location2 = document.getElementById('location1')
+
+let submit = document.getElementById("submitButton");
+
+
+
+submit.addEventListener("submit", (e) => {
+    e.preventDefault();
+  
+    
+console.log(cuisine1.value);
+console.log(cuisine2);
+console.log(location1);
+console.log(location2);
+
+    
+  })
+  

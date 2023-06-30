c193c92f95fd7b858c279702c58bfa27ccb88dcb
Muhammad Yahaya <101328329+Muhammad235@users.noreply.github.com>
2023-06-30 23:02:53 +0100

    ..

--- app.js
+++ app.js
@@ -7,16 +7,17 @@ let location2 = document.getElementById('location1')
 let submit = document.getElementById("submitButton");
 
 
+let cuisine1Value = cuisine1.value
 
 submit.addEventListener("submit", (e) => {
     e.preventDefault();
   
     
-console.log(cuisine1.value);
+console.log(cuisine1Value);
 console.log(cuisine2);
 console.log(location1);
 console.log(location2);
 
-    
-  })
+
+})
   
